36d7f44310438831f80099828d1a592d5dc62e57
Khethiwe-J-Dhlangamandla <kdhlangamandla@gmail.com>
2025-06-29 15:41:50 +0200

    Updated index.html and calculator.js
    Added a new function to the calculator.js file.

--- calculator.js
+++ calculator.js
@@ -1,14 +1,33 @@
-var newline = true; //global variable because it is outside of function and is a boolean.
+var newLine = true; //global variable because it is outside of function and is a boolean.
+var value1;
+var currentOperator;
 
+//Event handler for when any digit button is pressed
 function digitBtnPressed(button) {
   //function used to get digits to appear when clicked(function is called on index.html).
 
-  if (newline == true) {
+  if (newLine == true) {
     //this will aid to remove zero and have digits appear normaly
     document.getElementById("inputBox").value = button;
-    newline = false;
+    newLine = false;
   } else {
     var currentValue = document.getElementById("inputBox").value; //variable used for digits to appear on the input box.
     document.getElementById("inputBox").value = currentValue + button; //used for values to output/appear at once on the inputBox.
   }
 }
+
+//Event handler for when AC button is pressed
+function btnACPressed() {
+  //function that will reset input box and clear values when not needed, call it in html
+  document.getElementById("inputBox").value = 0;
+  newLine = true;
+}
+
+//Event handle for the operating signs when pressed.
+function operatorBtnPressed(operator) {
+  currentOperator = operator;
+  value1 = parseInt(document.getElementById("inputBox").value);
+  newLine = true;
+}
+//line29 parseInt is used as convertor from text to number value.
+//line29 also means = grab whatever output appears on inputbox, convert it to number and store it in value1.
